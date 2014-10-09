66fbe5db744fbb222567c199dc753a1e684e5165
stevenmarc <stevenmarcfr@gmail.com>
2014-10-09 09:54:44 +0200

    added users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,43 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_vfuser!, only: [:new, :create, :edit, :update, :destroy]
+ 
+ def index
+ 	 @users = User.all
+ end
+
+ def new
+    @user = User.new
+ end
+
+ def show
+ 	@user = User.find(params[:id])
+ end
+
+def edit
+ 	@user = User.find(params[:id])
+ end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User profile was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+	end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:username, :last_name, :first_name, :slug_line, :about, :country, :city, :address, :birthdate, :show_age, :show_email )
+    end	
+end
